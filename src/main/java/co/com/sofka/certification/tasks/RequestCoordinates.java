5e7bf33f9fd87f01ffcab12273a5c17cf18139d0
juan maya <juan.maya@sofka.com.co>
2022-08-30 13:22:44 -0500

    probando utilidad para hacer swipe en un target. falta probarlo

--- src/main/java/co/com/sofka/certification/tasks/RequestCoordinates.java
+++ src/main/java/co/com/sofka/certification/tasks/RequestCoordinates.java
@@ -1,22 +1,45 @@
 package co.com.sofka.certification.tasks;
 
-import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.Task;
-import net.serenitybdd.screenplay.Tasks;
-import net.serenitybdd.screenplay.actions.Click;
-
 import static co.com.sofka.certification.userinterfaces.CoordinatesUI.BUTTON_CONSULT_COORDINATES;
 
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.targets.Target;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.DriverTask;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.touch.TouchActions;
+
+import java.util.function.Consumer;
+
+
 public class RequestCoordinates implements Task {
+    private Actor actor;
+    private Object Consumer;
+
     @Override
     public <T extends Actor> void performAs(T actor) {
+        this.actor = actor;
         actor.attemptsTo(
-                Click.on(BUTTON_CONSULT_COORDINATES)
+                Click.on(BUTTON_CONSULT_COORDINATES),
+                swipeDown(BUTTON_CONSULT_COORDINATES)
         );
     }
 
-    public static RequestCoordinates current()
-    {
+    public static RequestCoordinates current() {
         return Tasks.instrumented(RequestCoordinates.class);
+    }
+
+    public Performable swipeDown(Target target) {
+        WebElement element = target.resolveFor(actor).getElement();
+        return new DriverTask(
+                driver -> {
+                    new TouchActions(driver).scroll(element, 0, 25).perform();
+                }
+        );
     }
 }
